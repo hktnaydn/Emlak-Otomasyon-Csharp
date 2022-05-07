f37a57ee58bc06b1a61efc023e3f1d716b5cb57d
hktnaydn <hktn.00.aydn@gmail.com>
2022-05-07 17:50:58 +0300

    Combobox'ların veritabanı ile dinamik çalışması

--- kayitemlak.Designer.cs
+++ kayitemlak.Designer.cs
@@ -35,6 +35,7 @@
             this.bunifuMaterialTextbox1 = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
@@ -46,7 +47,9 @@
             this.bunifuMaterialTextbox5 = new Bunifu.Framework.UI.BunifuMaterialTextbox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -84,6 +87,8 @@
             this.bunifuMaterialTextbox3.TabIndex = 12;
             this.bunifuMaterialTextbox3.Text = "Ev Oda Sayısı";
             this.bunifuMaterialTextbox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.bunifuMaterialTextbox3.Enter += new System.EventHandler(this.bunifuMaterialTextbox3_Enter);
+            this.bunifuMaterialTextbox3.Leave += new System.EventHandler(this.bunifuMaterialTextbox3_Leave);
             // 
             // bunifuMaterialTextbox2
             // 
@@ -104,6 +109,8 @@
             this.bunifuMaterialTextbox2.TabIndex = 11;
             this.bunifuMaterialTextbox2.Text = "Ev Toplam Alan";
             this.bunifuMaterialTextbox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.bunifuMaterialTextbox2.Enter += new System.EventHandler(this.bunifuMaterialTextbox2_Enter);
+            this.bunifuMaterialTextbox2.Leave += new System.EventHandler(this.bunifuMaterialTextbox2_Leave);
             // 
             // bunifuMaterialTextbox1
             // 
@@ -124,6 +131,8 @@
             this.bunifuMaterialTextbox1.TabIndex = 10;
             this.bunifuMaterialTextbox1.Text = "Kat Numarası";
             this.bunifuMaterialTextbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.bunifuMaterialTextbox1.Enter += new System.EventHandler(this.bunifuMaterialTextbox1_Enter);
+            this.bunifuMaterialTextbox1.Leave += new System.EventHandler(this.bunifuMaterialTextbox1_Leave);
             // 
             // label1
             // 
@@ -147,44 +156,58 @@
             this.panel1.Size = new System.Drawing.Size(1160, 100);
             this.panel1.TabIndex = 9;
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(1064, 13);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 63);
+            this.button2.TabIndex = 24;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // comboBox1
             // 
             this.comboBox1.BackColor = System.Drawing.Color.Crimson;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.comboBox1.ForeColor = System.Drawing.Color.White;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Daire",
-            "Müstakil",
-            "Dubleks",
-            "Apart"});
-            this.comboBox1.Location = new System.Drawing.Point(7, 188);
+            this.comboBox1.Location = new System.Drawing.Point(7, 223);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(218, 39);
             this.comboBox1.TabIndex = 20;
-            this.comboBox1.Text = "Ev Türü";
+            this.comboBox1.Tag = "";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.bunifuMaterialTextbox2);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.bunifuMaterialTextbox1);
             this.groupBox1.Controls.Add(this.bunifuMaterialTextbox3);
             this.groupBox1.Location = new System.Drawing.Point(64, 143);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(237, 241);
+            this.groupBox1.Size = new System.Drawing.Size(237, 278);
             this.groupBox1.TabIndex = 21;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Genel Bilgiler";
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.comboBox3);
             this.groupBox2.Controls.Add(this.comboBox2);
             this.groupBox2.Controls.Add(this.bunifuMaterialTextbox4);
             this.groupBox2.Location = new System.Drawing.Point(374, 143);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(351, 241);
+            this.groupBox2.Size = new System.Drawing.Size(351, 278);
             this.groupBox2.TabIndex = 22;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Adres Bilgileri";
@@ -192,35 +215,27 @@
             // comboBox3
             // 
             this.comboBox3.BackColor = System.Drawing.Color.Crimson;
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox3.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.comboBox3.ForeColor = System.Drawing.Color.White;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Items.AddRange(new object[] {
-            "Daire",
-            "Müstakil",
-            "Dubleks",
-            "Apart"});
-            this.comboBox3.Location = new System.Drawing.Point(18, 102);
+            this.comboBox3.Location = new System.Drawing.Point(18, 149);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(303, 39);
             this.comboBox3.TabIndex = 23;
-            this.comboBox3.Text = "İlçe";
             // 
             // comboBox2
             // 
             this.comboBox2.BackColor = System.Drawing.Color.Crimson;
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.comboBox2.ForeColor = System.Drawing.Color.White;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "İstanbul",
-            "Ankara",
-            "İzmir"});
-            this.comboBox2.Location = new System.Drawing.Point(18, 34);
+            this.comboBox2.Location = new System.Drawing.Point(18, 66);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(303, 39);
             this.comboBox2.TabIndex = 23;
-            this.comboBox2.Text = "İl";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // bunifuMaterialTextbox4
             // 
@@ -234,13 +249,15 @@
             this.bunifuMaterialTextbox4.LineIdleColor = System.Drawing.Color.Crimson;
             this.bunifuMaterialTextbox4.LineMouseHoverColor = System.Drawing.Color.BlueViolet;
             this.bunifuMaterialTextbox4.LineThickness = 3;
-            this.bunifuMaterialTextbox4.Location = new System.Drawing.Point(18, 158);
+            this.bunifuMaterialTextbox4.Location = new System.Drawing.Point(18, 208);
             this.bunifuMaterialTextbox4.Margin = new System.Windows.Forms.Padding(4);
             this.bunifuMaterialTextbox4.Name = "bunifuMaterialTextbox4";
-            this.bunifuMaterialTextbox4.Size = new System.Drawing.Size(303, 69);
+            this.bunifuMaterialTextbox4.Size = new System.Drawing.Size(303, 54);
             this.bunifuMaterialTextbox4.TabIndex = 0;
             this.bunifuMaterialTextbox4.Text = "ADRES";
             this.bunifuMaterialTextbox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.bunifuMaterialTextbox4.Enter += new System.EventHandler(this.bunifuMaterialTextbox4_Enter);
+            this.bunifuMaterialTextbox4.Leave += new System.EventHandler(this.bunifuMaterialTextbox4_Leave);
             // 
             // groupBox3
             // 
@@ -275,6 +292,8 @@
             this.bunifuMaterialTextbox6.TabIndex = 24;
             this.bunifuMaterialTextbox6.Text = "EK AÇIKLAMALAR";
             this.bunifuMaterialTextbox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.bunifuMaterialTextbox6.Enter += new System.EventHandler(this.bunifuMaterialTextbox6_Enter);
+            this.bunifuMaterialTextbox6.Leave += new System.EventHandler(this.bunifuMaterialTextbox6_Leave);
             // 
             // bunifuMaterialTextbox5
             // 
@@ -295,6 +314,8 @@
             this.bunifuMaterialTextbox5.TabIndex = 24;
             this.bunifuMaterialTextbox5.Text = "Fiyat";
             this.bunifuMaterialTextbox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.bunifuMaterialTextbox5.Enter += new System.EventHandler(this.bunifuMaterialTextbox5_Enter);
+            this.bunifuMaterialTextbox5.Leave += new System.EventHandler(this.bunifuMaterialTextbox5_Leave);
             // 
             // checkBox2
             // 
@@ -318,20 +339,38 @@
             this.checkBox1.Text = "Kiralık";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // label2
             // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.ForeColor = System.Drawing.Color.Transparent;
-            this.button2.Location = new System.Drawing.Point(1064, 13);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(78, 63);
-            this.button2.TabIndex = 24;
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(36, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 25);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Şehir";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(36, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 25);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "İlçe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(24, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 25);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Ev Türü";
             // 
             // kayitemlak
             // 
@@ -350,7 +389,9 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
@@ -376,5 +417,8 @@
         private Bunifu.Framework.UI.BunifuMaterialTextbox bunifuMaterialTextbox4;
         private Bunifu.Framework.UI.BunifuMaterialTextbox bunifuMaterialTextbox6;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
--- kayitemlak.cs
+++ kayitemlak.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 
 namespace emlakortomasyonu
 {
@@ -15,7 +18,70 @@ namespace emlakortomasyonu
         public kayitemlak()
         {
             InitializeComponent();
+            Fillcombo();
         }
+        MySqlConnection connection = Form1.baglanti;
+        private void kayitemlak_Load(object sender, EventArgs e)
+        {
+        }
+        void Fillcombo()
+        {
+            string constring = "datasource=localhost;port=3306;username=root;password=safranbolu78";
+            string QueryEvtype = "select * from emlakdatabase.evturleri;";
+            string QueryCity = "select * from emlakdatabase.sehirler;";
+            int sNameCityNo = 0;
+            MySqlConnection conDataBase = new MySqlConnection(constring);
+
+            // Ev türlerini Combobox a çekme
+            MySqlCommand cmdDataBase = new MySqlCommand(QueryEvtype, conDataBase);
+
+            // Şehirleri Combobox a çekme
+            MySqlCommand cmdDataBaseCity = new MySqlCommand(QueryCity, conDataBase);
+
+            MySqlDataReader MyReader;
+            try
+            {
+                conDataBase.Open();
+                MyReader = cmdDataBase.ExecuteReader();
+                while(MyReader.Read())
+                {
+                    String sName = MyReader.GetString("evturu");
+                    comboBox1.Items.Add(sName);
+                }
+                
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            conDataBase.Close();
+            // City
+
+
+            MySqlDataReader MyReaderCity;
+            try
+            {
+                conDataBase.Open();
+                MyReaderCity = cmdDataBaseCity.ExecuteReader();
+                while (MyReaderCity.Read())
+                {
+                    String sNameCity = MyReaderCity.GetString("sehirad");
+                    sNameCityNo = MyReaderCity.GetInt16("sehirno");
+                  
+                    comboBox2.Items.Add(sNameCity);
+                    
+                }
+
+            }
+            catch (Exception excity)
+            {
+                MessageBox.Show(excity.Message);
+            }
+            conDataBase.Close();
+
+        }
+
 
         private void groupBox3_Enter(object sender, EventArgs e)
         {
@@ -29,9 +95,145 @@ namespace emlakortomasyonu
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Form1 giris = new Form1();
-            giris.Show();
+            manageUsers lobi = new manageUsers();
+            lobi.Show();
             this.Close();
+        }
+
+        private void bunifuMaterialTextbox1_Enter(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox1.Text == "Kat Numarası")
+            {
+                bunifuMaterialTextbox1.Text = "";
+                bunifuMaterialTextbox1.ForeColor = Color.White;
+            }
+        }
+
+        private void bunifuMaterialTextbox1_Leave(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox1.Text == "")
+            {
+                bunifuMaterialTextbox1.Text = "Kat Numarası";
+                bunifuMaterialTextbox1.ForeColor = Color.Red;
+            }
+        }
+
+        private void bunifuMaterialTextbox2_Enter(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox2.Text == "Ev Toplam Alan")
+            {
+                bunifuMaterialTextbox2.Text = "";
+                bunifuMaterialTextbox2.ForeColor = Color.White;
+            }
+        }
+
+        private void bunifuMaterialTextbox2_Leave(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox2.Text == "")
+            {
+                bunifuMaterialTextbox2.Text = "Ev Toplam Alan";
+                bunifuMaterialTextbox2.ForeColor = Color.Red;
+            }
+        }
+
+        private void bunifuMaterialTextbox3_Enter(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox3.Text == "Ev Oda Sayısı")
+            {
+                bunifuMaterialTextbox3.Text = "";
+                bunifuMaterialTextbox3.ForeColor = Color.White;
+            }
+        }
+
+        private void bunifuMaterialTextbox3_Leave(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox3.Text == "")
+            {
+                bunifuMaterialTextbox3.Text = "Ev Oda Sayısı";
+                bunifuMaterialTextbox3.ForeColor = Color.Red;
+            }
+        }
+
+        private void bunifuMaterialTextbox5_Enter(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox5.Text == "Fiyat")
+            {
+                bunifuMaterialTextbox5.Text = "";
+                bunifuMaterialTextbox5.ForeColor = Color.Red;
+            }
+        }
+
+        private void bunifuMaterialTextbox5_Leave(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox5.Text == "")
+            {
+                bunifuMaterialTextbox5.Text = "Fiyat";
+                bunifuMaterialTextbox5.ForeColor = Color.Red;
+            }
+        }
+
+        private void bunifuMaterialTextbox4_Enter(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox4.Text == "ADRES")
+            {
+                bunifuMaterialTextbox4.Text = "";
+                bunifuMaterialTextbox4.ForeColor = Color.Red;
+            }
+        }
+
+        private void bunifuMaterialTextbox4_Leave(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox4.Text == "")
+            {
+                bunifuMaterialTextbox4.Text = "ADRES";
+                bunifuMaterialTextbox4.ForeColor = Color.Red;
+            }
+        }
+
+        private void bunifuMaterialTextbox6_Enter(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox6.Text == "EK AÇIKLAMALAR")
+            {
+                bunifuMaterialTextbox6.Text = "";
+                bunifuMaterialTextbox6.ForeColor = Color.Red;
+            }
+        }
+
+        private void bunifuMaterialTextbox6_Leave(object sender, EventArgs e)
+        {
+            if (bunifuMaterialTextbox6.Text == "")
+            {
+                bunifuMaterialTextbox6.Text = "EK AÇIKLAMALAR";
+                bunifuMaterialTextbox6.ForeColor = Color.Red;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int plaka=0;
+            comboBox3.Items.Clear();
+            connection.Open();
+            MySqlCommand secilensehirno = new MySqlCommand("select sehirno from emlakdatabase.sehirler where sehirad='" + comboBox2.Text+"'", connection);
+            MySqlDataReader reader = secilensehirno.ExecuteReader();
+            while (reader.Read())
+            {
+                
+                string sehirno = reader["sehirno"].ToString();
+                plaka = int.Parse(sehirno);
+                MessageBox.Show("Tebrikler hesabınız oluşturuldu! Müşteri numaranız : " + plaka.ToString(), "DÖVİZ AL-SAT");
+            }
+            connection.Close();
+            connection.Open();
+            MySqlCommand sehirnob = new MySqlCommand("select * from emlakdatabase.ilceler where sehirno='" + plaka + "'", connection);
+            MySqlDataReader readerr = sehirnob.ExecuteReader();
+            while (readerr.Read())
+            {
+                comboBox3.Items.Add(readerr["ilcead"].ToString());
+            }
+
+
+
+            connection.Close();
         }
     }
 }
